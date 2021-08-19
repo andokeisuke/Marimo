8bdcb6753abf578ff9dc3a7a492c858cbb82ecf0
andokeisuke <kakipi.moai@gmail.com>
2021-08-19 11:50:45 +0900

    firebase-connect

--- app/src/main/java/com/example/marimo/MainActivity.java
+++ app/src/main/java/com/example/marimo/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.marimo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +50,25 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplication(), MissionActivity.class);
             startActivity(intent);
         });
+
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        String msg = token;
+                        //Log.d(TAG, msg);
+                        //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
